b3a25010005c3da925862f346c853d588db87123
Recep <recep.90@gmail.com>
2014-03-29 18:31:34 +0200

    Proje Guncellendi

--- JavaProgramming/src/lib/cloudist/Test.java
+++ JavaProgramming/src/lib/cloudist/Test.java
@@ -12,6 +12,30 @@ public class Test {
 		
 		//Learning class obje oluştur
 		//print() metodunu çağırınız.
+		
+		Learning object_adi = new Learning();
+		object_adi.Print();
+		
+		
+		//Learning class ı içine methodlar yazacağız bunlar ; 
+		/*
+		 * -Kullanıcı tarafından girilen bir cümleyi;
+		 * -Kaç kelime var
+		 * -Kelimelerin içinde kaç sesli harf kaç tane sessiz harf var kaç noktalama işareti var kaç büyük harf kaç küçük harf var (ascii koduna göre karar verin.)
+		 * -Cümle içerisinde aradığımız kelimeyi bulabiliyor muyuz?
+		 * -Cümle de bir harf yada kelimeyi yanlış yazdık düzeltebiliyor muyuz?
+		 * -Cümleyi tersten yazdırabiliyor muyuz?
+		 * -Cümledeki kelimelere sayı verip kelimelerin yerini değiştirebiliyor muyuz?
+		 * -Cümle içine yeni kelime ekleyebiliyor muyuz?
+		 * -Cümle içinden kelime silebiliyor muyuz?
+		 * -Cümle içindeki tüm harflerin hepsini küçük yada büyük yapabiliyor muyuz?
+		 * -Cümle içindeki kelimeleri şifreleyebiliyor muyuz?
+		 * {	Alfabe de 26 harf var diyelim. Alfabe A harfinden başlıyor normalde. Mesela sizin alfabeniz D harfinden başlasın ve 
+		 * 		cümle içindeki her harfi normal sırasından 3 sıra kaydırın. Bu şekilde şifrenizi sizin dışınızda kimse çözemeyecek.
+		 * }
+		 * 
+		 * */
+		
 	}
 
 }
